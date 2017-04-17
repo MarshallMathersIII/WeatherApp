60c15c7f5276ff561754a426de2b49c089606345
eminem <ghy1060627080@163.com>
2017-04-17 11:24:26 +0800

    9：后台自动更新

--- app/src/main/java/com/eminem/weatherapp/WeatherActivity.java
+++ app/src/main/java/com/eminem/weatherapp/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.eminem.weatherapp;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.eminem.weatherapp.gson.Forecast;
 import com.eminem.weatherapp.gson.Weather;
+import com.eminem.weatherapp.service.AutoUpdateService;
 import com.eminem.weatherapp.util.HttpUtil;
 import com.eminem.weatherapp.util.Utility;
 
@@ -207,6 +209,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
 
